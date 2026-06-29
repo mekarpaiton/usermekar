7e8f97cace06c7940992fb5c58c33a420ee66904
mekarpaiton <mekarpaiton@gmail.com>
2026-06-29 09:11:11 +0700

    Update cart_item.dart

--- lib/providers/cart_item.dart
+++ lib/providers/cart_item.dart
@@ -1,15 +1,15 @@
 class CartItem {
   final String id;
   final String nama;
+  final int jumlah; // ← Pake 'jumlah' bukan 'qty'
   final int harga;
-  final String gambar;
-  int jumlah;
+  final String gambar; // ← Pake 'gambar' bukan 'imageUrl'
 
   CartItem({
     required this.id,
     required this.nama,
+    this.jumlah = 1, // Default 1
     required this.harga,
     required this.gambar,
-    this.jumlah = 1,
   });
 }
