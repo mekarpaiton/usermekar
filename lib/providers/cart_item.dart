977f49f5b4e49802f50164143c976cc43cc4e958
mekarpaiton <mekarpaiton@gmail.com>
2026-06-29 09:28:17 +0700

    Update cart_item.dart

--- lib/providers/cart_item.dart
+++ lib/providers/cart_item.dart
@@ -1,15 +1,24 @@
 class CartItem {
   final String id;
   final String nama;
-  final int jumlah; // ← Pake 'jumlah' bukan 'qty'
+  final int jumlah;
   final int harga;
-  final String gambar; // ← Pake 'gambar' bukan 'imageUrl'
+  final String gambar;
 
   CartItem({
     required this.id,
     required this.nama,
-    this.jumlah = 1, // Default 1
+    this.jumlah = 1,
     required this.harga,
     required this.gambar,
   });
+
+  // ← Tambahin ini
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'nama': nama,
+    'jumlah': jumlah,
+    'harga': harga,
+    'gambar': gambar,
+  };
 }
