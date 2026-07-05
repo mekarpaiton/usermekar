1d1f6d9b3b0e081034234f3f15c9f402d0860ceb
mekarpaiton <mekarpaiton@gmail.com>
2026-07-06 00:32:24 +0700

    Update cart_item.dart

--- lib/providers/cart_item.dart
+++ lib/providers/cart_item.dart
@@ -2,20 +2,16 @@ class CartItem {
   final String id;
   final String nama;
   final int harga;
-  final int hargaNormal;
-  final int isPromo;
   final String varian;
-  final String gambar; // <-- PAKAI gambar BUKAN foto
+  final String gambar;
   int qty;
 
   CartItem({
     required this.id,
     required this.nama,
     required this.harga,
-    required this.hargaNormal,
-    required this.isPromo,
     required this.varian,
-    required this.gambar, // <-- gambar
+    required this.gambar,
     this.qty = 1,
   });
 
@@ -23,10 +19,8 @@ class CartItem {
     'id': id,
     'nama': nama,
     'harga': harga,
-    'harga_normal': hargaNormal,
-    'is_promo': isPromo,
     'varian': varian,
-    'gambar': gambar, // <-- gambar
+    'gambar': gambar,
     'qty': qty,
   };
 }
