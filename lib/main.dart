3bd53a45d1680d9b169045b8d5c2af3105ca2c32
mekarpaiton <mekarpaiton@gmail.com>
2026-07-06 00:45:25 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/material.dart';
+iimport 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:badges/badges.dart' as badges;
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:provider/provider.dart';
+
 import 'providers/cart_provider.dart';
 import 'pages/halaman_checkout.dart';
 import 'pages/cek_order_page.dart';
